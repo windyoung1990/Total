5bcb701bf10910db2caf55050811bec9d2c96250
windyoung1990 <1078156801@qq.com>
2017-08-03 18:18:04 +0800

    demo2

---
+++ Webpack/Demos/demo2/main.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+ReactDOM.render(
+	<h1>Hello</h1>,
+	document.querySelector("#wrapper")
+)
+
+
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.querySelector('#wrapper')
+// );
